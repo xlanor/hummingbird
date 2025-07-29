9dae5ee2f8c4e726cac7a2a345b0bad9aa33f1e0
143mailliw <wwokwilliam@gmail.com>
2025-07-28 17:57:31 -0700

    feat(scan): use windows libraries for default paths

--- src/settings/scan.rs
+++ src/settings/scan.rs
@@ -18,27 +18,45 @@ impl Default for ScanSettings {
 }
 
 fn retrieve_default_paths() -> Vec<PathBuf> {
-    if let Some(user_directories) = directories::UserDirs::new() {
-        if let Some(dir) = user_directories.audio_dir() {
-            if exists(dir).unwrap_or(false) {
-                return vec![dir.into()];
-            } else {
-                warn!("Music directory doesn't exist: nothing will be scanned by default.");
-            }
-        } else {
-            let dir = user_directories.home_dir().join("Music");
-            warn!("Music directory couldn't be discovered normally, using $HOME/Music.");
-            if exists(&dir).unwrap_or(false) {
-                return vec![dir];
-            } else {
-                warn!("$HOME/Music doesn't exist: nothing will be scanned by default.");
-            }
-        };
-    } else {
-        error!("Couldn't find your home directory.");
-        warn!("Nothing will be scanned by default, and no config files will be loadable.");
-        warn!("Please create a home directory for this user.");
+    #[cfg(target_os = "windows")]
+    {
+        use windows::Storage::{KnownLibraryId, StorageLibrary};
+
+        StorageLibrary::GetLibraryAsync(KnownLibraryId::Music)
+            .unwrap()
+            .get()
+            .unwrap()
+            .Folders()
+            .unwrap()
+            .into_iter()
+            .map(|v| v.Path().unwrap().to_string().into())
+            .collect()
     }
 
-    vec![]
+    #[cfg(not(target_os = "windows"))]
+    {
+        if let Some(user_directories) = directories::UserDirs::new() {
+            if let Some(dir) = user_directories.audio_dir() {
+                if exists(dir).unwrap_or(false) {
+                    return vec![dir.into()];
+                } else {
+                    warn!("Music directory doesn't exist: nothing will be scanned by default.");
+                }
+            } else {
+                let dir = user_directories.home_dir().join("Music");
+                warn!("Music directory couldn't be discovered normally, using $HOME/Music.");
+                if exists(&dir).unwrap_or(false) {
+                    return vec![dir];
+                } else {
+                    warn!("$HOME/Music doesn't exist: nothing will be scanned by default.");
+                }
+            };
+        } else {
+            error!("Couldn't find your home directory.");
+            warn!("Nothing will be scanned by default, and no config files will be loadable.");
+            warn!("Please create a home directory for this user.");
+        }
+
+        vec![]
+    }
 }
