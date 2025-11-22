5b6f8e89a83664b87f644cc889e0e4dd068c389b
Cameron Ward <cameronuward@gmail.com>
2025-11-22 03:54:25 -0700

    build: reduce spurious rebuilds due to build script
    emitting `rerun-if-changed` for a file that doesn't exist means cargo
    will always rerun the build script (it assumes it's been deleted since
    last run). this has been the soruce of my pain this whole time, simply
    because i don't have a `.env`. frustratingly, this is intended behavior:
    https://doc.rust-lang.org/cargo/faq.html#why-is-cargo-rebuilding-my-code
    if we just don't emit it when `.env` isn't present, it wouldn't get rerun
    upon being created, so all of the obvious unintrusive solutions are out.
    
    instead, create an empty `.env` if the file doesn't exist already. this
    will unfortunately induce a single unnecessary rerun, since the newly
    created file's mtime > build script start time, but i'm not so psychotic
    that i'd set it myself just to avoid 5s once. also, one unnecessary rerun
    is better than it happening EVERY SINGLE TIME, so...
    
    fixes three bugs: the original script effectively loads the envfile into
    environment variables twice; duplicate entries now use the first value
    they're set to (upstream behavior, dont look at me 🤷); and we no longer
    overwrite the current environment's vars, e.g. incase the user wants to
    override what's set in `.env` (if that was the intended behavior, just
    change it to `from_read_override` - that way it's explicitly defined)
    
    also adds `anyhow` as a build-dependency, though as you can see this was
    already transitively the case thru `vergen`

--- build.rs
+++ build.rs
@@ -1,21 +1,24 @@
 use vergen_git2::{Emitter, Git2Builder};
 
-fn main() {
-    println!("cargo:rerun-if-changed=.env",);
-    let dotpath = dotenvy::dotenv();
+fn read_or_create_envfile() -> std::io::Result<String> {
+    std::fs::File::options()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(".env")
+        .and_then(std::io::read_to_string)
+}
 
-    if dotpath.is_ok() {
-        for env_var in dotenvy::dotenv_iter().unwrap() {
-            let (key, value) = env_var.unwrap();
-            println!("cargo:rustc-env={key}={value}");
-        }
+fn main() -> anyhow::Result<()> {
+    let envfile = read_or_create_envfile()?;
+    println!("cargo:rerun-if-changed=.env");
+    dotenvy::from_read(envfile.as_bytes())?;
+    let mut vars = dotenvy::from_read_iter(envfile.as_bytes());
+    while let Some((key, _)) = vars.next().transpose()? {
+        println!("cargo:rustc-env={key}={}", std::env::var(&key)?);
     }
 
-    let flags = Git2Builder::default().sha(true).build().unwrap();
-
-    Emitter::default()
-        .add_instructions(&flags)
-        .unwrap()
-        .emit()
-        .unwrap();
+    let flags = Git2Builder::default().sha(true).build()?;
+    Emitter::default().add_instructions(&flags)?.emit()
 }
