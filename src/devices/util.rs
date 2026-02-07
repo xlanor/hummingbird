268bff067faeb7815edb0c378b57b6bb1dd400b8
143mailliw <wwokwilliam@gmail.com>
2026-02-06 20:01:35 -0700

    refactor: remove pack() allocations

--- src/devices/util.rs
+++ src/devices/util.rs
@@ -3,38 +3,17 @@ use std::sync::atomic::AtomicU64;
 
 use super::resample::{SampleFrom, SampleInto};
 
-#[allow(dead_code)]
-pub fn interleave<T>(samples: Vec<Vec<T>>) -> Vec<T>
-where
-    T: Copy + PartialEq,
-{
-    if samples.is_empty() {
-        return vec![];
-    }
-
-    let length = samples.len();
-    let mut result = vec![];
-
-    for i in 0..(samples.len() * samples[0].len()) {
-        result.push(samples[i % length][i / length]);
-    }
-
-    result
-}
-
 // Code is dead on non-Linux platforms only
 #[allow(dead_code)]
 pub trait Packed {
-    fn pack(&self) -> Vec<u8>;
+    fn pack(&self) -> impl Iterator<Item = u8>;
 }
 
 macro_rules! impl_packed {
     ($t:ty) => {
         impl Packed for [$t] {
-            fn pack(&self) -> Vec<u8> {
-                self.iter()
-                    .flat_map(|&x| x.to_ne_bytes().to_vec())
-                    .collect()
+            fn pack(&self) -> impl Iterator<Item = u8> {
+                self.iter().flat_map(|&x| x.to_ne_bytes())
             }
         }
     };
@@ -52,8 +31,8 @@ impl_packed!(f64);
 
 // special cases
 impl Packed for [u8] {
-    fn pack(&self) -> Vec<u8> {
-        self.to_vec()
+    fn pack(&self) -> impl Iterator<Item = u8> {
+        self.iter().copied()
     }
 }
 
