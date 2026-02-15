d8b3ae7da02b299ef0e33ec46b0293d95d5b3978
William Whittaker <wwokwilliam@gmail.com>
2026-02-15 02:45:16 -0700

    fix: delete album/artist when they are orphaned

---
+++ migrations/20260215002300_add_track_update_cleanup_trigger.sql
@@ -0,0 +1,31 @@
+CREATE TRIGGER IF NOT EXISTS update_track_album_cleanup AFTER UPDATE OF album_id ON track
+WHEN OLD.album_id IS NOT NULL AND (NEW.album_id IS NULL OR OLD.album_id != NEW.album_id)
+BEGIN
+    DELETE FROM album_path
+    WHERE
+        album_path.path = OLD.folder
+        AND album_path.disc_num = IFNULL(OLD.disc_number, -1)
+        AND album_path.album_id = OLD.album_id
+        AND NOT EXISTS (
+            SELECT 1
+            FROM track
+            WHERE track.folder = OLD.folder
+              AND track.disc_number IS OLD.disc_number
+              AND track.album_id = OLD.album_id
+        );
+
+    DELETE FROM album
+    WHERE album.id = OLD.album_id
+    AND NOT EXISTS (
+        SELECT 1
+        FROM track
+        WHERE track.album_id = OLD.album_id
+    );
+
+    DELETE FROM artist
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM album
+        WHERE album.artist_id = artist.id
+    );
+END;
--- queries/scan/get_album_id.sql
+++ queries/scan/get_album_id.sql
@@ -1,1 +1,1 @@
-SELECT id FROM album WHERE title = $1 AND mbid = $2;
+SELECT id FROM album WHERE title = $1 AND mbid = $2 AND artist_id IS $3;
